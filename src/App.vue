09d701f019765119034f9f36ebe480148e3b4b5f
wwhtml <1129942397@qq.com>
2023-05-07 03:23:35 +0800

    初始化开发环境

---
+++ src/App.vue
@@ -0,0 +1,78 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div>
+    <el-button>ssss</el-button>
+    <ul>
+      <li>sss</li>
+      <li>sss</li>
+      <li>sss</li>
+      <li>sss</li>
+      <li>sss</li>
+      <li>sss</li>
+      <li>sss</li>
+      <li>sss</li>
+      <li>sss</li>
+      <li>sss</li>
+      <li>sss</li>
+      <li>sss</li>
+      <li>sss</li>
+      <li>sss</li>
+      <li>sss</li>
+      <li>sss</li>
+      <li>sss</li>
+      <li>sss</li>
+      <li>sss</li>
+      <li>sss</li>
+      <li>sss</li>
+      <li>sss</li>
+      <li>sss</li>
+      <li>sss</li>
+      <li>sss</li>
+      <li>sss</li>
+      <li>sss</li>
+      <li>sss</li>
+      <li>sss</li>
+    </ul>
+    <ul>
+      <li>sss</li>
+      <li>sss</li>
+      <li>sss</li>
+      <li>sss</li>
+      <li>sss</li>
+      <li>sss</li>
+      <li>sss</li>
+      <li>sss</li>
+      <li>sss</li>
+      <li>sss</li>
+      <li>sss</li>
+      <li>sss</li>
+      <li>sss</li>
+      <li>sss</li>
+      <li>sss</li>
+      <li>sss</li>
+      <li>sss</li>
+      <li>sss</li>
+      <li>sss</li>
+      <li>sss</li>
+      <li>sss</li>
+      <li>sss</li>
+      <li>sss</li>
+      <li>sss</li>
+      <li>sss</li>
+      <li>sss</li>
+      <li>sss</li>
+      <li>sss</li>
+      <li>sss</li>
+    </ul>
+    <a href="">dddd</a>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+ul {
+  li {
+    color: $base-color;
+  }
+}
+</style>
